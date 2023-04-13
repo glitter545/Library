997b400f41b55e367c29c26c7e57a1ffb67beec8
glitter545 <bobbybroken992@gmail.com>
2023-04-13 03:47:34 -0700

    Create displayBooks function to render books in the list

--- script.js
+++ script.js
@@ -27,3 +27,45 @@ function removeBook(e) {
   myLibrary.splice(index, 1);
   displayBooks();
 }
+
+function displayBooks() {
+    bookList.innerHTML = "";
+  
+    myLibrary.forEach((book, index) => {
+      const row = document.createElement("tr");
+  
+      const titleCell = document.createElement("td");
+      titleCell.textContent = book.title;
+      row.appendChild(titleCell);
+  
+      const authorCell = document.createElement("td");
+      authorCell.textContent = book.author;
+      row.appendChild(authorCell);
+  
+      const pagesCell = document.createElement("td");
+      pagesCell.textContent = book.pages;
+      row.appendChild(pagesCell);
+  
+      const readCell = document.createElement("td");
+      const readButton = document.createElement("button");
+      readButton.textContent = book.read ? "Read" : "Unread";
+      readButton.classList.add(book.read ? "read" : "unread");
+      readButton.addEventListener("click", () => {
+        book.toggleRead();
+        displayBooks();
+      });
+      readCell.appendChild(readButton);
+      row.appendChild(readCell);
+  
+      const actionsCell = document.createElement("td");
+      const removeButton = document.createElement("button");
+      removeButton.textContent = "Remove";
+      removeButton.setAttribute("data-index", index);
+      removeButton.addEventListener("click", removeBook);
+      actionsCell.appendChild(removeButton);
+      actionsCell.appendChild(document.createTextNode(" "));
+      row.appendChild(actionsCell);
+  
+      bookList.appendChild(row);
+    });
+  }
